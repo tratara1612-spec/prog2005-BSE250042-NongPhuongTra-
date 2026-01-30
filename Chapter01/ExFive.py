e9ef8a69073ff16e28b5f9a765804dbd0412a893
tratara1612-spec <tratara1612@gmail.com>
2026-01-30 16:41:49 +0700

    Create ExFive.py

---
+++ Chapter01/ExFive.py
@@ -0,0 +1,11 @@
+name = int(input("Nhập tên”)
+age = int(input("Nhập tuổi”)
+average_score = 8.5
+age_next_year = age + 1
+doubled_score = average_score * 2
+print("--- Thông tin cá nhân ---")
+print(f"Tên: {name} | Kiểu: {type(name)}")
+print(f"Tuổi: {age} | Kiểu: {type(age)}")
+print(f"Điểm TB: {average_score} | Kiểu: {type(average_score)}")
+print(f"Tuổi sang năm: {age_next_year} | Kiểu: {type(age_next_year)}")
+print(f"Điểm nhân đôi: {doubled_score} | Kiểu: {type(doubled_score)}")
