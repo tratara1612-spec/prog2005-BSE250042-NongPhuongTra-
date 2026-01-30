46325d3429c41e1191f8736678978609d6f488e0
tratara1612-spec <tratara1612@gmail.com>
2026-01-30 16:38:21 +0700

    Create ExThree.py

---
+++ Chapter01/ExThree.py
@@ -0,0 +1,9 @@
+a = int(input("Nhập số nguyên thứ nhất: "))
+b = int(input("Nhập số nguyên thứ hai: "))
+print(f"Tổng: {a + b}")
+print(f"Hiệu: {a - b}")
+print(f"Tích: {a * b}")
+if b != 0:
+    print(f"Thương: {a / b}")
+else:
+    print("Không thể chia cho 0")
