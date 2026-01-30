c4d884f34bc12f3a8b70cf5c0284bad410cc05d5
tratara1612-spec <tratara1612@gmail.com>
2026-01-30 16:55:49 +0700

    Delete ExFifteen.py

--- ExFifteen.py
+++
@@ -1,8 +0,0 @@
-for i in range(1, 4):
-    print(f"\n--- Nhập thông tin sinh viên {i} ---")
-    ten = input("Tên sinh viên: ")
-    toan = float(input("Điểm Toán: "))
-    ly = float(input("Điểm Lý: "))
-    hoa = float(input("Điểm Hóa: ")) 
-    dtb = (toan + ly + hoa) / 3
-    print(f"Sinh viên: {ten} | Điểm trung bình: {round(dtb, 2)}")
