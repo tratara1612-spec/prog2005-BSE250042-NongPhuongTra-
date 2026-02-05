603a3b3371e31ca69f153e2c43f8b9d97e6da6ff
tratara1612-spec <tratara1612@gmail.com>
2026-02-05 07:30:07 +0700

    Create  ExEleven.py

---
+++ Chapter02/ExEleven.py
@@ -0,0 +1,10 @@
+diem = float(input("Nhập điểm trung bình: "))
+
+if diem >= 8.0:
+    print("Xếp loại: Giỏi")
+elif diem >= 6.5:
+    print("Xếp loại: Khá")
+elif diem >= 5.0:
+    print("Xếp loại: Trung bình")
+else:
+    print("Xếp loại: Yếu")
