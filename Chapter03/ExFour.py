5bf83c9f49452aac764c0f9164305cff11cda8e8
tratara1612-spec <tratara1612@gmail.com>
2026-03-05 09:54:35 +0700

    Create ExFour.py

---
+++ Chapter03/ExFour.py
@@ -0,0 +1,8 @@
+danh_sach=[2, 23, 64, 12, 78, 49]
+print(f'Danh sách ban đầu: {danh_sach}')
+danh_sach.sort()
+print(f'Danh sách theo thứ tự tăng dần: {danh_sach}')
+danh_sach.reverse()
+print(f'Danh sách đảo ngược:{danh_sach}')
+danh_sach.count(2)
+print(f'Số lần xuất hiện của số 2: {danh_sach.count(2)}')
