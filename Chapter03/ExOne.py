74f7459238dffebd3d17734f503a6c68b32c50ab
tratara1612-spec <tratara1612@gmail.com>
2026-03-05 09:36:17 +0700

    Update ExOne.py

--- Chapter03/ExOne.py
+++ Chapter03/ExOne.py
@@ -1,4 +1,4 @@
-mon_hoc=('Anh', 'Cơ sở lập trình', 'E-learning', 'Đại só')
+mon_hoc=['Anh', 'Cơ sở lập trình', 'E-learning', 'Đại só']
 print(f'Số lượng môn học là: {len(mon_hoc)}')
 print('\n---Danh sách môn học---')
 for mon in mon_hoc:
