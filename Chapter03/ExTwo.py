834d94ca38b5eb9bb4200b5e2d9e23ca0f625164
tratara1612-spec <tratara1612@gmail.com>
2026-03-05 09:35:53 +0700

    Create ExTwo.py

---
+++ Chapter03/ExTwo.py
@@ -0,0 +1,6 @@
+ban_be= ['An', 'Linh', 'Nhi']
+print(f'Danh sách ban đầu: {ban_be}')
+ban_be.append('Dung')
+print(f'Danh sách sau khi thêm 1 người bạn mới: {ban_be}')
+ban_be.pop(1)
+print(f'Danh sách sau khi xóa bạn ỏ vị trí thứ hai: {ban_be}')
