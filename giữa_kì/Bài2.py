7961524a3eef25a1bfdbe782b2776e4b3d25fd13
tratara1612-spec <tratara1612@gmail.com>
2026-03-09 17:30:11 +0700

    Create Bài2.py

---
+++ giữa_kì/Bài2.py
@@ -0,0 +1,9 @@
+a = int(input('Nhập hệ số a: '))
+b = int(input('Nhập hệ số b: '))
+c = int(input('Nhập hệ số c: '))
+
+lon_nhat = max(a, b, c)
+nho_nhat = min(a, b, c)
+
+print(f'Số lớn nhất là:{lon_nhat}')
+print(f'Số bé nhất là: {nho_nhat}')
