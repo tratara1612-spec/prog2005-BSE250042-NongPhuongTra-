6343bdd31b616fbf0c8f7936f7f793aff67165c5
tratara1612-spec <tratara1612@gmail.com>
2026-03-13 18:01:08 +0700

    Update Bai10.py

--- LT_1/Bai10.py
+++ LT_1/Bai10.py
@@ -3,14 +3,14 @@ FILE_NAME = 'qlds_sanpham.txt'
 
 
 def nhapSP():
-    print("\n---Nhập thông tin cho từng sản phẩm---")
-    ma = input("Mẫ số sản phẩm: ")
+    print('\n---Nhập thông tin cho từng sản phẩm---')
+    ma = input('Mẫ số sản phẩm: ')
     ten = input ('Tên sản phẩm: ')
-    gia = float(input("Giá sản phẩm: "))
+    gia = float(input('Giá sản phẩm: '))
 
     with open(FILE_NAME, 'a', encoding="utf-8") as f:
-        f.write(f"{ma},{ten},{gia}\n")
-    print("---Đã lưu sản phẩm thành công---")
+        f.write(f'{ma},{ten},{gia}\n')
+    print('---Đã lưu sản phẩm thành công---')
 
 
 def docFile():
@@ -31,7 +31,7 @@ def docFile():
 
 def HienThi(ds):
     if not ds:
-        print("Danh sách trống!")
+        print('Danh sách trống!')
         return
     print(f"{'Mã số sản phẩm':<15}|{'Tên sản phẩm':<30}|{'Giá tiền':<20}")
     print("-"*80)
@@ -42,25 +42,25 @@ def HienThi(ds):
 def SapXepDsGiam():
     ds = docFile()
     ds_sorted = sorted(ds, key=lambda x: x['gia'], reverse=True)
-    print("\n---Danh sách sau khi sắp xếp theo giá giảm dần---")
+    print('\n---Danh sách sau khi sắp xếp theo giá giảm dần---')
     HienThi(ds_sorted)
 
 while True:
-    print("\n---MENU QUẢN LÝ SẢN PHẨM---")
-    print("1. Nhập sản phẩm mới ")
-    print("2. Hiển thị danh sách sản phẩm ")
-    print("3. Sắp xếp theo giá gảm dần")
-    print("0. Thoát! ")
-    chon = input("Lựa chọn của bạn là: ")
+    print('\n---MENU QUẢN LÝ SẢN PHẨM---')
+    print('1. Nhập sản phẩm mới ')
+    print('2. Hiển thị danh sách sản phẩm ')
+    print('3. Sắp xếp theo giá gảm dần')
+    print('0. Thoát! ')
+    chon = input('Lựa chọn của bạn là: ')
 
     if chon == "1":
         nhapSP()
     elif chon == "2":
-        print("Danh sách sản phẩm hiện có")
+        print('Danh sách sản phẩm hiện có')
         HienThi(docFile())
     elif chon == "3":
         SapXepDsGiam()
     elif chon == "0":
         break
     else:
-        print("Vui lòng chọn lại!")
+        print('Vui lòng chọn lại!')
