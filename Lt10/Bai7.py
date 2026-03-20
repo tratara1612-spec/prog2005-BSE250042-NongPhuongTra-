e528b031452dc0388bae231004e8daeccddba999
tratara1612-spec <tratara1612@gmail.com>
2026-03-20 16:54:22 +0700

    Create Bai7.py

---
+++ Lt10/Bai7.py
@@ -0,0 +1,8 @@
+tu_khoa = 'python123'
+nhap = ''
+while nhap != tu_khoa:
+    nhap = input('Nhập mật khẩu của bạn: ')
+    if nhap != tu_khoa:
+        print('Sai mật khẩu!')
+
+print('Đăng nhập thành công!')
