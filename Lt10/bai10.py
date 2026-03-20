a32a8608e64d074d28440a77ce7f8ce743367e33
tratara1612-spec <tratara1612@gmail.com>
2026-03-20 17:23:17 +0700

    Create bai10.py

---
+++ Lt10/bai10.py
@@ -0,0 +1,23 @@
+def bubblue_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] < arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+                print(f'Đổi chỗ: {arr}')
+
+        if not swapped:
+            break
+    return arr
+ds = []
+for i in range(5):
+    ds.append(input(f'Nhập chuỗi {i+1}: '))
+
+print("\n---Quá trình sắp xếp---")
+ket_qua = bubblue_sort(ds)
+
+print("\n---Kết quả cuối cùng (Giảm dần)--- ")
+for s in ket_qua:
+    print(f"'{s}' - Độ dài: {len(s)}")
