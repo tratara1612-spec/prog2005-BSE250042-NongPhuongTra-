c118aee31977dd291e6777efe75efce40e665980
tratara1612-spec <tratara1612@gmail.com>
2026-04-06 17:04:58 +0700

    Create Bai1.py

---
+++ LT15/Bai1.py
@@ -0,0 +1,9 @@
+a = int(input('Nhập số nguyên thứ nhất: '))
+b = int(input('Nhập số nguyên thứ hai: '))
+print('Tổng', a+b)
+print('Hiệu', a-b)
+print('Tích', a*b)
+if b != 0:
+    print('Thương', a/b)
+else:
+    print('Không thể chia hết cho 0!')
